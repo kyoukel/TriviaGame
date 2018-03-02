78da242a69ceaa172567a84ecf982947e44603dd
kyoukel <kimyoukel@gmail.com>
2018-03-02 14:37:13 -0800

    created index2.html file linked to start button to display the questions page. created index3.html file linked to submit button to change page to display the results page. created radio buttons form with only 1 click for each answer.

---
+++ assets/javascript/app.js
@@ -0,0 +1,60 @@
+// WHAT'S NEEDED FOR THE GAME LOGIC
+
+// ---> BACKGROUND PICTURE - CARTOONS & LINK IMAGES FILE
+// ---> ADD CSS & LINK CSS FILE
+
+        //You'll create a trivia form with multiple choice or true/false options (your choice).
+            //create inputs with radio buttons in html 
+
+            //The player will have a limited amount of time to finish the quiz.
+            //player chooses answers
+                //log the players choice
+                //compare the players choice to the correct answer
+
+            //The game ends when the time runs out.The page will reveal the number of questions that players answer correctly and incorrectly.
+            //  Don't let the player pick more than one answer per question.
+
+            //Don't forget to include a countdown timer.
+
+// 1. 1st Page HTML***
+// ---> Game Title
+// ---> Start Button 
+
+// 2. 2nd Page *** When Start button is clicked...
+// ---> Displays index2.html file
+
+    //=========START BUTTON CLICK FUNCTION ()============
+
+// ---> CALL COUNTDOWN TIMER ()
+// ---> Timer is displayed on screen
+
+    //==========COUNTDOWN TIMER FUNCTION ()==============
+    function myFunction() {
+        alert('Hello');
+    }
+
+// 3. User has 120 seconds to choose an answer for all 5 questions.
+// ---> DISPLAYS INDEX2.HTML = Questions with radio button multiple choices displayed.
+// ---> user can only choose 1 answer per question.
+
+// 4. INDEX3.HTML *** When countdown timer reaches 0....
+// ---> Displays INDEX2.HTML with right, wrong, & missed answers.
+// ---> Hides 2nd page html.
+
+// 5. Create counter that logs the user's choices for...
+// ---> adds correct answers
+// ---> adds incorrect answers
+// ---> adds missed answers
+
+    //=======COUNTER FUNCTION THAT ADDS CHOICES========
+
+// 6. Compares the players choice to the correct answer and...
+// ---> Inserts the total at the html ID targeted for each...
+// ---> correct ID
+// ---> incorrect ID
+// ---> missed ID
+
+// ============ IF / ELSE IF STATEMENTS THAT COMPARE & INSERT ANSWERS==================
+
+
+
